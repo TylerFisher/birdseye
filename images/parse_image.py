0e0fec755f8cf3150ac4a9ee9b8ffdfe5f0afe78
Tyler Fisher <tyler@tylerjfisher.com>
2015-04-08 16:45:13 -0400

    parse a directory of images, store in data structure

--- images/parse_image.py
+++ images/parse_image.py
@@ -1,13 +1,30 @@
+from glob import glob
+import json
+
 from libxmp import consts
 from libxmp.utils import file_to_dict
 
-xmp = file_to_dict('nprborderlands_0043.jpg')
-dc = xmp[consts.XMP_NS_DC]
+images = []
 
-for item in dc:
-	if item[0].startswith('dc:subject') and item[1] != '':
-		print item[1]
-	if item[0].startswith('dc:description') and item[1] != '':
-		print item[1]
-	if item[0].startswith('dc:creator') and item[1] != '':
-		print item[1]
+for image_path in glob('jpg/*.jpg'):
+
+	filename = image_path.split('/')[1]
+	image = {
+		'filename': filename,
+		'tags': []
+	}
+
+	xmp = file_to_dict(image_path)
+	dc = xmp[consts.XMP_NS_DC]
+
+	for item in dc:
+		if item[0].startswith('dc:subject') and item[1] != '':
+			image['tags'].append(item[1])
+		if item[0].startswith('dc:description') and item[1] != '' and item[1] != 'x-default':
+			image['caption'] = item[1]
+		if item[0].startswith('dc:creator') and item[1] != '':
+			image['creator'] = item[1]
+
+	images.append(image)
+
+print images
