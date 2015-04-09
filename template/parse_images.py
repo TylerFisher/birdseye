822272fc206ec7bf142abd2495bdb6cef793d4a7
Tyler Fisher <tyler@tylerjfisher.com>
2015-04-09 15:21:42 -0400

    move parsing out of fab

---
+++ template/parse_images.py
@@ -0,0 +1,32 @@
+from glob import glob
+import json
+
+from libxmp import consts
+from libxmp.utils import file_to_dict
+
+images = []
+
+for image_path in glob('www/assets/*.jpg'):
+
+    filename = image_path.split('/')[2]
+    image = {
+        'filename': filename,
+        'tags': []
+    }
+
+    xmp = file_to_dict(image_path)
+    dc = xmp[consts.XMP_NS_DC]
+
+    for item in dc:
+        if item[1] != '':
+            if item[0].startswith('dc:subject'):
+                image['tags'].append(item[1])
+            if item[0].startswith('dc:description') and item[1] != 'x-default':
+                image['caption'] = item[1]
+            if item[0].startswith('dc:creator'):
+                image['creator'] = item[1]
+
+    images.append(image)
+
+with open('data/images.json', 'w') as f:
+    f.write(json.dumps(images))
